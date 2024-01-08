e4bc379eef6a425749b0e5d278e9b52dd7f10232
Huskiez <admin@huskatronics.com>
2024-01-08 18:48:15 +1000

    day15 part 1

--- cmd/day15/main.go
+++ cmd/day15/main.go
@@ -1,5 +1,35 @@
 package main
 
-func main() {
+import (
+	"aoc2023-golang/pkg/readinput"
+	"fmt"
+)
 
+func main() {
+	data := readinput.ReadInput("input.txt", ",")
+	result1 := solve1(data)
+	fmt.Printf("Answer for part 1: %d\n", result1)
+}
+
+func solve1(data []string) int {
+	totalSum := 0
+	for _, item := range data {
+		value := getHashValue(item)
+		totalSum += value
+	}
+	return totalSum
+}
+
+func getHashValue(word string) int {
+	total := 0
+	for _, character := range word {
+		//making sure it's utf-8
+		if int(character) > 255 {
+			continue
+		}
+		total += int(character)
+		total *= 17
+		total = total % 256
+	}
+	return total
 }
